f3f7aee945e72e968050f621837d4a753dac0309
TotalBro <cantaloupeman@gmail.com>
2019-03-06 16:01:09 -0600

    All relevant files added.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,54 @@
+function makebuttons () {
+    for (let i = 0; i < 14; i++) {
+        let listElement = document.createElement('li');
+        let box = document.createElement('button');
+              box.setAttribute('class','button');
+              box.setAttribute('id', makeId(i));
+              box.setAttribute('')
+
+        listElement.appendChild(box);      
+
+        document.querySelector('.containerList').appendChild(listElement);
+    }
+}
+
 function add (numOne, numTwo) {
     return numOne + numTwo;
 }
 
+function subtract (numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply (numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide (numOne, numTwo) {
+    if (numTwo == 0) {
+        return "Can't divide by zero!";
+    }
+    else {
+        return numOne/numtwo;
+    }
+}
+
+function operate (operator, numOne, numTwo) {
+    switch (operator) {
+        case '*':
+            return multiply (numOne, numTwo);
+        case '/':
+            return divide(numOne, numTwo);
+        case '+':
+            return add(numOne, numTwo);
+        case '-':
+            return subtract(numOne, numTwo);
+        default:
+            return 'Something went real bad wrong.'        
+    }
+}
 
 module.exports = {
-	translate
+    add,
+    subtract
 }
--- calculator.spec.js
+++ calculator.spec.js
@@ -5,4 +5,49 @@ describe('#add', function() {
 		s = calculator.add(2,3);
 		expect(s).toEqual(5);
 	});
+	it('adds two negative numbers', function() {
+		s = calculator.add(-2,-3);
+		expect(s).toEqual(-5);
+	});
+});
+
+describe('#subtract', function() {
+	it('subtracts numbers', function() {
+		s = calculator.subtract(2,3);
+		expect(s).toEqual(-1);
+	});
+	it('subtracts negative numbers', function() {
+		s = calculator.subtract(-2,-3);
+		expect(s).toEqual(1);
+	});
+});
+
+describe('#multiply', function() {
+	it('multiplies numbers', function() {
+		s = calculator.multiply(2,3);
+		expect(s).toEqual(6);
+	});
+	it('multiplies negative numbers', function() {
+		s = calculator.subtract(-2,-3);
+		expect(s).toEqual(6);
+	});
+});
+
+describe('#divide', function() {
+	xit('divides numbers', function() {
+		s = calculator.divide(4,2);
+		expect(s).toEqual(2);
+	});
+	xit('divides negative numbers', function() {
+		s = calculator.divide(-4,-2);
+		expect(s).toEqual(2);
+	});
+	xit('rounds to two decimal places', function() {
+		s = calculator.divide(3,4);
+		expect(s).toEqual(0.75);
+	});
+	xit('disallows divide by zero', function() {
+		s = calculator.divide(4,0);
+		expect(s).toEqual("Can't divide by zero!");	
+	});
 });
